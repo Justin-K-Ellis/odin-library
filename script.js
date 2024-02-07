62bf95b811837b639cbbdf2480438ccdb3cde597
Justin <jlke45@gmail.com>
2024-02-07 22:09:40 +0900

    Create new html page with Boostrap

--- script.js
+++ script.js
@@ -36,51 +36,76 @@ function handleForm(event) {
 function displayBooks() {
     removeAllChildNodes(innerGallery);
     for (book of library) {
-        cardBuilder(book);
+        // cardBuilder(book);
+        appendCard(book);
     }
 }
 
-function cardBuilder(book) {
-    // Create card elements
-    let outerCard = document.createElement("div");
-    let card = document.createElement("div");
-    let cardContent = document.createElement("div");
-    let cardTitle = document.createElement("span");
-    let cardAuthor = document.createElement("p");
-    let cardYear = document.createElement("p");
-    let cardPages = document.createElement("p");
-
-    // Give elements content
-    cardTitle.textContent = book.title;
-    cardAuthor.textContent = `Author: ${book.author}`;
-    cardYear.textContent = book.year;
-    cardPages.textContent = `${book.pages} pages`;
-
-    // Apply CSS classes to elements
-    outerCard = addOuterCard(outerCard);
-    card.classList.add("card");
-    cardContent.classList.add("card-content");
-    cardTitle.classList.add("card-title");
-
-    // Append elements to elements
-    cardContent.appendChild(cardTitle);
-    cardContent.appendChild(cardAuthor);
-    cardContent.appendChild(cardYear);
-    cardContent.appendChild(cardPages);
-    card.appendChild(cardContent);
-    outerCard.appendChild(card);
-
-    // Append finished card to parent div
-    innerGallery.appendChild(outerCard);
+function appendCard(book) {
+    const html = makeCardHTML(book);
+    const card = document.createElement("div");
+    // card.classList.add("row");
+    card.innerHTML = html;
+    innerGallery.appendChild(card);
 }
 
-// Add multiple CSS classes to outer card
-function addOuterCard(div) {
-    div.classList.add("col");
-    div.classList.add("s12");
-    div.classList.add("m3");
-    return div;
+function makeCardHTML(book) {
+    const cardHTML = `
+        <div class="card col" style="width: 18rem;">
+            <div class="card-header">
+                <h5><em>${book.title}</em></h5>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">By ${book.author}</li>
+                <li class="list-group-item">Published in ${book.year}</li>
+                <li class="list-group-item">${book.pages} pages</li>
+            </ul>
+        </div>
+    `
+    return cardHTML;
 }
+
+// function cardBuilder(book) {
+//     // Create card elements
+//     let outerCard = document.createElement("div");
+//     let card = document.createElement("div");
+//     let cardContent = document.createElement("div");
+//     let cardTitle = document.createElement("span");
+//     let cardAuthor = document.createElement("p");
+//     let cardYear = document.createElement("p");
+//     let cardPages = document.createElement("p");
+
+//     // Give elements content
+//     cardTitle.textContent = book.title;
+//     cardAuthor.textContent = `Author: ${book.author}`;
+//     cardYear.textContent = book.year;
+//     cardPages.textContent = `${book.pages} pages`;
+
+//     // Apply CSS classes to elements
+//     outerCard = addOuterCard(outerCard);
+//     card.classList.add("card");
+//     cardContent.classList.add("card-content");
+//     cardTitle.classList.add("card-title");
+
+//     // Append elements to elements
+//     cardContent.appendChild(cardTitle);
+//     cardContent.appendChild(cardAuthor);
+//     cardContent.appendChild(cardYear);
+//     cardContent.appendChild(cardPages);
+//     card.appendChild(cardContent);
+//     outerCard.appendChild(card);
+
+//     // Append finished card to parent div
+//     innerGallery.appendChild(outerCard);
+// }
+
+// // Add multiple CSS classes to outer card
+// function addOuterCard(div) {
+//     div.classList.add("col");
+//     div.classList.add("s12");
+//     div.classList.add("m3");
+//     return div;
+// }
 
 
 
@@ -92,15 +117,7 @@ function removeAllChildNodes(parent) {
 }
 
 // Example book
-const republic = new Book("Republic", "Plato", "375 BCE", 295);
-library.push(republic);
+// const republic = new Book("Republic", "Plato", "375 BCE", 295);
+// library.push(republic);
 displayBooks();
-
-// TODO: implement accordian
-
-// Accordian
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.collapsible');
-//     var instances = M.Collapsible.init(elems, true);
-//   });
       
