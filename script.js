f7d15f3b3c8fb118ccb622455deb2967947e12ac
Justin <jlke45@gmail.com>
2024-02-10 21:51:04 +0900

    Refactor makeCardHTML() to makeRow()
    Rather than using a template literal to create the HTML, this (the data
    about the book) element is now created using DOM manipulation.

--- script.js
+++ script.js
@@ -39,27 +39,48 @@ function displayBooks() {
 }
 
 function appendCard(book) {
-    const card = makeCardHTML(book);   
-    tableBody.appendChild(card);
+    const row = makeRow(book);   
+    tableBody.appendChild(row);
 }
 
-function makeCardHTML(book) {
-    const cardHTML = `
-        <td scope="row"><em>${book.title}</em></td>
-        <td>${book.author}</td>
-        <td>${book.year}</td>
-        <td>${book.pages}</td>
-        <td>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                <label class="form-check-label" for="flexCheckDefault">
-                </label>
-            </div>
-        </td>
-    `
-    let card = document.createElement("tr");
-    card.innerHTML = cardHTML;
-    return card;
+function makeRow(book) {
+    let row = document.createElement("tr");
+
+    let titleData = document.createElement("td");
+    titleData.setAttribute("scope", "row");
+    let authorData = document.createElement("td");
+    let yearData = document.createElement("td");
+    let pagesData = document.createElement("td");
+    let readData = document.createElement("td");
+
+    let formCheckData = document.createElement("td");
+    let formCheck = document.createElement("div");
+    formCheck.classList.add("form-check");
+    let input = document.createElement("input");
+    input.classList.add("form-check-input");
+    input.setAttribute("type", "checkbox");
+    input.setAttribute("value", "");
+    input.id = "flexCheckDefault";
+    let label = document.createElement("label");
+    label.classList.add("form-check-label");
+    label.setAttribute("for", input.id);
+
+    formCheck.appendChild(input);
+    formCheck.appendChild(label);
+    formCheckData.appendChild(formCheck);
+
+    titleData.textContent = book.title;
+    authorData.textContent = book.author;
+    yearData.textContent = book.year;
+    pagesData.textContent = book.pages;
+    
+    row.appendChild(titleData);
+    row.appendChild(authorData);
+    row.appendChild(yearData);
+    row.appendChild(pagesData);
+    row.appendChild(formCheckData);
+
+    return row;
 }
 
 // Clear displayed books in order to display new books
